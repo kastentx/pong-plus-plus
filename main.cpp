513683c57957692524424415918c47288c8140f1
Nick Kasten <kastencode@gmail.com>
2016-10-08 16:06:26 -0500

    fleshing out cBall functions and Move switch statement

--- main.cpp
+++ main.cpp
@@ -1,11 +1,72 @@
 #include <iostream>
 using namespace std;
 
+enum eDir {STOP = 0, LEFT = 1, UPLEFT = 2, DOWNLEFT = 3, RIGHT = 4, UPRIGHT = 5, DOWNRIGHT = 6};
+
 class cBall
 {
 private:
 	int x, y;
 	int originalX, originalY;
+	eDir direction;
+public:
+	cBall(int posX, int posY)
+	{
+		originalX = posX;
+		originalY = posY;
+		x = posX;
+		y = posY;
+		direction = STOP;
+	}
+	void Reset()
+	{
+		x = originalX;
+		y = originalY;
+		direction = STOP;
+	}
+	void changeDirection(eDir d)
+	{
+		direction d;
+	}
+	void randomDirection()
+	{
+		direction = (eDir)((rand() % 6) + 1);
+	}
+	void Move()
+	{
+		switch (direction)
+		{
+			case STOP:
+				break;
+			case LEFT:
+				x--;
+				break;
+			case RIGHT:
+				x++;
+				break;
+			case UPLEFT:
+				x--;
+				y--;
+				break;
+			case DOWNLEFT:
+				x--;
+				y++;
+				break;
+			case UPRIGHT:
+				x++;
+				y--;
+				break;
+			case DOWNRIGHT:
+				x++;
+				y++;
+				break;
+			default:
+				break;
+		}
+	}
+	inline int getX() { return x; }
+	inline int getY() { return y; }
+	inline eDir getDirection() { return eDir; }
 }
 
 int main() 
